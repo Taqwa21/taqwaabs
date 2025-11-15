61c56d41325431b23b92e7fac8aea5f2ab4a844b
Taqwa21 <taqwa.r1928@gmail.com>
2025-11-15 11:52:57 +0700

    New

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/* tailwind.config.js */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./*.html", "./assets/script/**/*.js"],
   theme: {
     extend: {},
   },
